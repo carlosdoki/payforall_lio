4f103ee6db776af0b28b97784640838a5918ed90
Camila Doki <camiladoki@Camilas-MacBook-Pro.local>
2017-07-29 23:28:29 -0300

    first commit

---
+++ app/src/main/java/com/example/camiladoki/pay4all_lio/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.camiladoki.pay4all_lio;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgView;
+    Button gerarQR;
+    EditText valorText;
+    String EditTextValue;
+    Thread thread;
+    public final static int QRcodeWidth = 500;
+    Bitmap bitmap;
+    String latitude;
+    String longitude;
+    String CID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gerarQR = (Button) findViewById(R.id.gerarQR);
+        valorText = (EditText) findViewById(R.id.valorText);
+        imgView = (ImageView) findViewById(R.id.imgView);
+
+    /* Use the LocationManager class to obtain GPS locations */
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener mlocListener = new MyLocationListener();
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+
+        gerarQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final TelephonyManager telephony = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                if (telephony.getPhoneType() == TelephonyManager.PHONE_TYPE_GSM) {
+                    final GsmCellLocation location = (GsmCellLocation) telephony.getCellLocation();
+                    if (location != null) {
+                        CID = String.valueOf(location.getCid());
+                    }
+                }
+                EditTextValue = valorText.getText().toString() + ";lat=" + latitude + ";lon=" + longitude + ";IDFV=" + CID + ";contrato=123;vendedor=1";
+
+                try {
+                    bitmap = TextToImageEncode(EditTextValue);
+
+                    imgView.setImageBitmap(bitmap);
+
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+    Bitmap TextToImageEncode(String Value) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    QRcodeWidth, QRcodeWidth, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                        getResources().getColor(R.color.QRCodeBlackColor) : getResources().getColor(R.color.QRCodeWhiteColor);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
+    }
+    /* Class My Location Listener */
+    public class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location loc) {
+
+            loc.getLatitude();
+            loc.getLongitude();
+
+            //String Text = "My current location is: " +
+            //        "Latitud = " + loc.getLatitude() +
+            //        "Longitud = " + loc.getLongitude();
+
+            latitude = String.valueOf(loc.getLatitude());
+            longitude = String.valueOf(loc.getLongitude());
+            //Toast.makeText(getApplicationContext(), Text, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            //Toast.makeText(getApplicationContext(), "Gps Disabled", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            //Toast.makeText(getApplicationContext(), "Gps Enabled", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+
+    }
+}
